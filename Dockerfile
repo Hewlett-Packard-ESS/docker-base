55fe74189c3dd36db9f579774ee44ddabaef30f1
Karl Stoney <karl.stoney@hp.com>
2015-03-19 15:52:09 +0000

    improvements to trapping

--- Dockerfile
+++ Dockerfile
@@ -28,16 +28,18 @@ RUN groupadd -g 1250 docker && \
     mkdir -p /storage && \
     chown docker:docker /storage
 
+# Disable requiretty on sudo as we use it for context switching
+RUN echo "Defaults !requiretty" >> /etc/sudoers
+
 # Add the supervisor configuration files and launch it
 COPY supervisord.conf /etc/supervisord.conf
 COPY preboot/* /preboot/
 
-COPY scripts/hpess_shell.sh /etc/profile.d/hpess_shell.sh
-COPY scripts/start.sh /usr/local/bin/start.sh
-COPY scripts/funcs.sh /usr/local/bin/funcs.sh
+# Copy over our scripts and shell
+COPY scripts/* /usr/local/bin/
+RUN ln -s /usr/local/bin/hpess_shell.sh /etc/profile.d/hpess_shell.sh
 
 WORKDIR /storage
 ENV HPESS_ENV base
  
 ENTRYPOINT ["/usr/local/bin/start.sh"]
-
--- scripts/start.sh
+++ scripts/start.sh
@@ -26,52 +26,54 @@ if [ "$#" -gt 0 ]; then
     set -e
     echo " => Executing: $*"
     $*
+
 else
 
     function finish {
-    debug "Detected SIGTERM, Gracefully Shutting Down..."
-    sleep 2
-    if kill -s 0 $parent >/dev/null 2>&1; then
-        debug "Forwarding SIGTERM to Sub Shell PID: $child..."
-        (sleep 0.5; kill -TERM $child >/dev/null 2>&1) &
-        wait $parent
-        exit_code=$?
-        debug "Sudo exited with Exit Code: $exit_code"
-        exit $exit_code
-    else
-        debug "Parent pid not running, usually because of it capturing signals itself.  As a result exit code will be set to 143."
-        exit 143
-    fi
-}
-trap finish TERM INT
+        debug "Detected SIGTERM, Gracefully Shutting Down..."
+        sleep 2
+        if kill -s 0 $parent >/dev/null 2>&1; then
+            debug "Forwarding SIGTERM to Sub Shell PID: $child..."
+            (sleep 0.5; kill -TERM $child >/dev/null 2>&1) &
+            wait $parent
+            exit_code=$?
+            debug "Sudo exited with Exit Code: $exit_code"
+            exit $exit_code
+        else
+            debug "Parent pid not running, usually because of it capturing signals itself.  As a result exit code will be set to 143."
+            exit 143
+        fi
+    }
+    trap finish TERM INT
 
-SERVICES=`ls /etc/supervisord.d | wc -l`
-if [ "$SERVICES" -eq "1" ] || [ "$FORCE_SUPERVISOR" == "true" ]; then
-    SERVICE_FILE=`ls -1 /etc/supervisord.d`
-    SERVICE_NAME=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i '\[program:' | awk -F'program:' '{print "["$2}'`
-    SERVICE_USER=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'user=' | awk -F'user=' '{print $2}'`
-    SERVICE_USER=${SERVICE_USER:-root}
-    SERVICE_COMMAND=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'command=' | awk -F'command=' '{print $2}'`
-    SERVICE_ENV=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'environment' | awk -F'environment=' '{print $2}'`
-    if [ ! "$SERVICE_ENV" == "" ]; then
-        SERVICE_ENV=${SERVICE_ENV//\"/}
-        declare ${SERVICE_ENV//,/ }
-        export ${SERVICE_ENV//,/ }
+    SERVICES=`ls /etc/supervisord.d | wc -l`
+    if [ "$SERVICES" -eq "1" ] || [ "$FORCE_SUPERVISOR" == "true" ]; then
+        SERVICE_FILE=`ls -1 /etc/supervisord.d`
+        SERVICE_NAME=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i '\[program:' | awk -F'program:' '{print "["$2}'`
+        SERVICE_USER=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'user=' | awk -F'user=' '{print $2}'`
+        SERVICE_USER=${SERVICE_USER:-root}
+        SERVICE_COMMAND=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'command=' | awk -F'command=' '{print $2}'`
+        SERVICE_ENV=`cat /etc/supervisord.d/$SERVICE_FILE | grep -i 'environment' | awk -F'environment=' '{print $2}'`
+        if [ ! "$SERVICE_ENV" == "" ]; then
+            SERVICE_ENV=${SERVICE_ENV//\"/}
+            declare ${SERVICE_ENV//,/ }
+            export ${SERVICE_ENV//,/ }
+        fi
+        info "Only a single process defined in supervisord.  Starting $SERVICE_NAME directly!"
+        debug "Executing '$SERVICE_COMMAND' as $SERVICE_USER"
+    elif [ "$SERVICES" -gt "1" ]; then
+        info "Multiple processes defined in supervisor config, starting supervisord..."
+        SERVICE_USER=root
+        SERVICE_COMMAND="supervisord -c /etc/supervisord.conf"
+    else
+        warn "There are no defined services in /etc/supervisord.d, skipping supervisor startup!"
+        exit 0
     fi
-    info "Only a single process defined in supervisord.  Starting $SERVICE_NAME directly!"
-    debug "Executing '$SERVICE_COMMAND' as $SERVICE_USER"
-elif [ "$SERVICES" -gt "1" ]; then
-    info "Multiple processes defined in supervisor config, starting supervisord..."
-    SERVICE_USER=root
-    SERVICE_COMMAND="supervisord -c /etc/supervisord.conf"
-else
-    warn "There are no defined services in /etc/supervisord.d, skipping supervisor startup!"
-    exit 0
-fi
-(sudo -E -H -u $SERVICE_USER bash -c "source /usr/local/bin/funcs.sh && $SERVICE_COMMAND") &
-parent=$!
-sleep 0.5
-child=$(pgrep -P $parent )
-debug "Sudo PID: $parent, Sub Shell PID: $child"
-wait $parent
+
+    sudo -E -H -u $SERVICE_USER env PATH=$PATH bash -c "source /usr/local/bin/funcs.sh; $SERVICE_COMMAND" &
+    parent=$!
+    sleep 0.5
+    child=$(pgrep -P $parent )
+    debug "Sudo PID: $parent, Sub Shell PID: $child"
+    wait $parent
 fi
